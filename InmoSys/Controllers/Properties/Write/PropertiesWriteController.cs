ce5ed2ed468a62cfc213a7473dc02ed50f3eb2e5
marcas1216 <projectmarcas1216@hotmail.com>
2025-08-30 07:34:54 -0500

    Task 41: Crear solución Aplicar Clean Arquitecture: Propiedades y Dueños

---
+++ InmoSys/Controllers/Properties/Write/PropertiesWriteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InmoSys.Controllers.Properties.Write
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertiesWriteController : ControllerBase
+    {
+    }
+}
---
+++ InmoSys/Controllers/Users/Read/UsersReadController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InmoSys.Controllers.Users.Read
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersReadController : ControllerBase
+    {
+    }
+}
