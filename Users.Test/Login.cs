4ca0f448dbbf9843bb13c85bcd9eab26306bb518
marcas1216 <projectmarcas1216@hotmail.com>
2025-08-30 06:21:08 -0500

    Task 41: Crear solución Aplicar Clean Arquitecture: Módulo de Usuarios

---
+++ Users.Test/Login.cs
@@ -0,0 +1,16 @@
+namespace Users.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
